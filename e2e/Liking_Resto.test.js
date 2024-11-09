ce8ac7a99a2635126276d40aacd39951e7b3762b
Mutiara <mutiarapandejlaki27@gmail.com>
2024-11-09 23:00:35 +0800

    e2e test liking resto

--- e2e/Liking_Resto.test.js
+++ e2e/Liking_Resto.test.js
@@ -10,3 +10,25 @@ Before (({ I }) => {
 Scenario('menampilkan page favorite kosong',  ({ I }) => {
   I.see('', '.favorite .list-resto');
 });
+
+const assert = require('assert');
+
+Scenario('suka satu resto', async ({ I }) => {
+  I.see('', '.favorite .list-resto');
+
+  I.amOnPage('/');
+
+  I.seeElement('.list-resto .button .detailBtn a');
+  const firstResto = locate('.list-resto .desc h3');
+  const firstRestoTitle = await I.grabTextFrom(firstResto);
+  I.click(locate('.list-resto .button .detailBtn a'));
+
+  I.seeElement('#likeBtn #likeButton');
+  I.click('#likeBtn #likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.favorite .list-resto');
+  const likedRestoTitle = await I.grabTextFrom('.favorite .list-resto .item-resto .desc h3');
+
+  assert.strictEqual(firstRestoTitle, likedRestoTitle);
+});
