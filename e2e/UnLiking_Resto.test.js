38bd7c402314cac8808416bdde4eb8440f3968c4
Mutiara <mutiarapandejlaki27@gmail.com>
2024-11-10 00:01:07 +0800

    e2e test unliking resto

--- e2e/UnLiking_Resto.test.js
+++ e2e/UnLiking_Resto.test.js
@@ -1,10 +1,10 @@
 /* eslint-disable func-call-spacing */
 /* eslint-disable no-undef */
-Feature('Liking Resto');
+Feature('UnLiking Resto');
 
 Before (({ I }) => {
   I.amOnPage('/#/favorite');
-  I.wait(2);
+  I.wait(1);
 });
 
 Scenario('menampilkan page favorite kosong',  ({ I }) => {
@@ -13,10 +13,12 @@ Scenario('menampilkan page favorite kosong',  ({ I }) => {
 
 const assert = require('assert');
 
-Scenario('suka satu resto', async ({ I }) => {
+Scenario('suka satu resto kemudian batal suka resto', async ({ I }) => {
   I.see('', '.favorite .list-resto');
+  I.wait(1);
 
   I.amOnPage('/');
+  I.wait(1);
 
   I.seeElement('.list-resto .button .detailBtn a');
   const firstResto = locate('.list-resto .desc h3');
@@ -27,11 +29,20 @@ Scenario('suka satu resto', async ({ I }) => {
 
   I.seeElement('#likeBtn #likeButton');
   I.click('#likeBtn #likeButton');
+  I.wait(2);
 
   I.amOnPage('/#/favorite');
   I.seeElement('.favorite .list-resto');
   I.wait(1);
   const likedRestoTitle = await I.grabTextFrom('.favorite .list-resto .item-resto .desc h3');
 
+  I.seeElement('.favorite .list-resto .item-resto .descItem .button .detailBtn a');
+  I.click('.favorite .list-resto .item-resto .descItem .button .detailBtn a');
+
+  I.seeElement('#likeBtn #likedButton');
+  I.click('#likeBtn #likedButton');
+
+  I.amOnPage('/#/favorite');
+  I.wait(2);
   assert.strictEqual(firstRestoTitle, likedRestoTitle);
 });
